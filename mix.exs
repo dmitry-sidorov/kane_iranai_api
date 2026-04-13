8f9031e485f7d1781ad31dc40026ef30a77e937c
dmitry-sidorov <jimmaggot@mail.ru>
2026-04-13 12:44:01 +0300

    Add guardian module

--- mix.exs
+++ mix.exs
@@ -51,7 +51,9 @@ defmodule KaneIranaiApi.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:guardian, "~> 2.4"},
+      {:bcrypt_elixir, "~> 3.3"}
     ]
   end
 
